673a6e67b0077735ea113830160945e8269584cc
Shingo Otsuka <shingo.otsuka@fixstars.com>
2018-12-25 19:06:44 +0900

    Fix indent

--- modules/cudastereo/src/cuda/stereosgm_census_transform.cu
+++ modules/cudastereo/src/cuda/stereosgm_census_transform.cu
@@ -22,87 +22,87 @@ limitations under the License.
 
 namespace cv { namespace cuda { namespace device
 {
-namespace stereosgm
-{
+    namespace stereosgm
+    {
 
-static constexpr int WINDOW_WIDTH  = 9;
-static constexpr int WINDOW_HEIGHT = 7;
+        static constexpr int WINDOW_WIDTH  = 9;
+        static constexpr int WINDOW_HEIGHT = 7;
 
-static constexpr int BLOCK_SIZE = 128;
-static constexpr int LINES_PER_BLOCK = 16;
+        static constexpr int BLOCK_SIZE = 128;
+        static constexpr int LINES_PER_BLOCK = 16;
 
-template <typename T>
-__global__ void census_transform_kernel(
-    PtrStepSz<T> src,
-    PtrStepSz<uint32_t> dest)
-{
-    using pixel_type = T;
-    static const int SMEM_BUFFER_SIZE = WINDOW_HEIGHT + 1;
+        template <typename T>
+        __global__ void census_transform_kernel(
+            PtrStepSz<T> src,
+            PtrStepSz<uint32_t> dest)
+        {
+            using pixel_type = T;
+            static const int SMEM_BUFFER_SIZE = WINDOW_HEIGHT + 1;
 
-    const int half_kw = WINDOW_WIDTH  / 2;
-    const int half_kh = WINDOW_HEIGHT / 2;
+            const int half_kw = WINDOW_WIDTH  / 2;
+            const int half_kh = WINDOW_HEIGHT / 2;
 
-    __shared__ pixel_type smem_lines[SMEM_BUFFER_SIZE][BLOCK_SIZE];
+            __shared__ pixel_type smem_lines[SMEM_BUFFER_SIZE][BLOCK_SIZE];
 
-    const int tid = threadIdx.x;
-    const int x0 = blockIdx.x * (BLOCK_SIZE - WINDOW_WIDTH + 1) - half_kw;
-    const int y0 = blockIdx.y * LINES_PER_BLOCK;
+            const int tid = threadIdx.x;
+            const int x0 = blockIdx.x * (BLOCK_SIZE - WINDOW_WIDTH + 1) - half_kw;
+            const int y0 = blockIdx.y * LINES_PER_BLOCK;
 
-    for(int i = 0; i < WINDOW_HEIGHT; ++i){
-        const int x = x0 + tid, y = y0 - half_kh + i;
-        pixel_type value = 0;
-        if(0 <= x && x < src.cols && 0 <= y && y < src.rows){
-            value = src(y, x);
-        }
-        smem_lines[i][tid] = value;
-    }
-    __syncthreads();
-
-#pragma unroll
-    for(int i = 0; i < LINES_PER_BLOCK; ++i){
-        if(i + 1 < LINES_PER_BLOCK){
-            // Load to smem
-            const int x = x0 + tid, y = y0 + half_kh + i + 1;
-            pixel_type value = 0;
-            if(0 <= x && x < src.cols && 0 <= y && y < src.rows){
-                value = src(y, x);
+            for(int i = 0; i < WINDOW_HEIGHT; ++i){
+                const int x = x0 + tid, y = y0 - half_kh + i;
+                pixel_type value = 0;
+                if(0 <= x && x < src.cols && 0 <= y && y < src.rows){
+                    value = src(y, x);
+                }
+                smem_lines[i][tid] = value;
             }
-            const int smem_x = tid;
-            const int smem_y = (WINDOW_HEIGHT + i) % SMEM_BUFFER_SIZE;
-            smem_lines[smem_y][smem_x] = value;
-        }
+            __syncthreads();
 
-        if(half_kw <= tid && tid < BLOCK_SIZE - half_kw){
-            // Compute and store
-            const int x = x0 + tid, y = y0 + i;
-            if(half_kw <= x && x < src.cols - half_kw && half_kh <= y && y < src.rows - half_kh){
-                const int smem_x = tid;
-                const int smem_y = (half_kh + i) % SMEM_BUFFER_SIZE;
-                uint32_t f = 0;
-                for(int dy = -half_kh; dy < 0; ++dy){
-                    const int smem_y1 = (smem_y + dy + SMEM_BUFFER_SIZE) % SMEM_BUFFER_SIZE;
-                    const int smem_y2 = (smem_y - dy + SMEM_BUFFER_SIZE) % SMEM_BUFFER_SIZE;
-                    for(int dx = -half_kw; dx <= half_kw; ++dx){
-                        const int smem_x1 = smem_x + dx;
-                        const int smem_x2 = smem_x - dx;
-                        const auto a = smem_lines[smem_y1][smem_x1];
-                        const auto b = smem_lines[smem_y2][smem_x2];
-                        f = (f << 1) | (a > b);
+        #pragma unroll
+            for(int i = 0; i < LINES_PER_BLOCK; ++i){
+                if(i + 1 < LINES_PER_BLOCK){
+                    // Load to smem
+                    const int x = x0 + tid, y = y0 + half_kh + i + 1;
+                    pixel_type value = 0;
+                    if(0 <= x && x < src.cols && 0 <= y && y < src.rows){
+                        value = src(y, x);
+                    }
+                    const int smem_x = tid;
+                    const int smem_y = (WINDOW_HEIGHT + i) % SMEM_BUFFER_SIZE;
+                    smem_lines[smem_y][smem_x] = value;
+                }
+
+                if(half_kw <= tid && tid < BLOCK_SIZE - half_kw){
+                    // Compute and store
+                    const int x = x0 + tid, y = y0 + i;
+                    if(half_kw <= x && x < src.cols - half_kw && half_kh <= y && y < src.rows - half_kh){
+                        const int smem_x = tid;
+                        const int smem_y = (half_kh + i) % SMEM_BUFFER_SIZE;
+                        uint32_t f = 0;
+                        for(int dy = -half_kh; dy < 0; ++dy){
+                            const int smem_y1 = (smem_y + dy + SMEM_BUFFER_SIZE) % SMEM_BUFFER_SIZE;
+                            const int smem_y2 = (smem_y - dy + SMEM_BUFFER_SIZE) % SMEM_BUFFER_SIZE;
+                            for(int dx = -half_kw; dx <= half_kw; ++dx){
+                                const int smem_x1 = smem_x + dx;
+                                const int smem_x2 = smem_x - dx;
+                                const auto a = smem_lines[smem_y1][smem_x1];
+                                const auto b = smem_lines[smem_y2][smem_x2];
+                                f = (f << 1) | (a > b);
+                            }
+                        }
+                        for(int dx = -half_kw; dx < 0; ++dx){
+                            const int smem_x1 = smem_x + dx;
+                            const int smem_x2 = smem_x - dx;
+                            const auto a = smem_lines[smem_y][smem_x1];
+                            const auto b = smem_lines[smem_y][smem_x2];
+                            f = (f << 1) | (a > b);
+                        }
+                        dest(y, x) = f;
                     }
                 }
-                for(int dx = -half_kw; dx < 0; ++dx){
-                    const int smem_x1 = smem_x + dx;
-                    const int smem_x2 = smem_x - dx;
-                    const auto a = smem_lines[smem_y][smem_x1];
-                    const auto b = smem_lines[smem_y][smem_x2];
-                    f = (f << 1) | (a > b);
-                }
-                dest(y, x) = f;
+                __syncthreads();
             }
         }
-        __syncthreads();
-    }
-}
 
         void censusTransform(const cv::cuda::GpuMat& src, cv::cuda::GpuMat& dest, cv::cuda::Stream& _stream)
         {
