4fd34975881006105ad52496d4c6c25603711502
Shingo Otsuka <shingo.otsuka@fixstars.com>
2019-01-08 16:24:44 +0900

    Add root of Path Aggregation while using OpenCV's structures

--- modules/cudastereo/src/cuda/stereosgm_path_aggregation.hpp
+++ modules/cudastereo/src/cuda/stereosgm_path_aggregation.hpp
@@ -17,40 +17,15 @@ limitations under the License.
 #ifndef SGM_PATH_AGGREGATION_HPP
 #define SGM_PATH_AGGREGATION_HPP
 
-#include "device_buffer.hpp"
-#include "types.hpp"
+#include "opencv2/core/cuda.hpp"
 
-namespace sgm {
-
-template <size_t MAX_DISPARITY>
-class PathAggregation {
-
-private:
-	static const unsigned int NUM_PATHS = 8;
-
-	DeviceBuffer<cost_type> m_cost_buffer;
-	cudaStream_t m_streams[NUM_PATHS];
-	cudaEvent_t m_events[NUM_PATHS];
-
-public:
-	PathAggregation();
-	~PathAggregation();
-
-	const cost_type *get_output() const {
-		return m_cost_buffer.data();
-	}
-
-	void enqueue(
-		const feature_type *left,
-		const feature_type *right,
-		int width,
-		int height,
-		unsigned int p1,
-		unsigned int p2,
-		cudaStream_t stream);
-
-};
-
-}
+namespace cv { namespace cuda { namespace device
+{
+    namespace stereosgm
+    {
+        template <size_t MAX_DISPARITY>
+        void pathAggregation(const GpuMat& left, const GpuMat& right, GpuMat& dest, Stream& stream);
+    }
+}}}
 
 #endif
