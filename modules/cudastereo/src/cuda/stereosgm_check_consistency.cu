0c400eb598d328c367a71bdd792e77fd24db521d
Shingo Otsuka <shingo.otsuka@fixstars.com>
2019-04-08 14:44:26 +0900

    Fix check consistency using OpenCV structure

--- modules/cudastereo/src/cuda/stereosgm_check_consistency.cu
+++ modules/cudastereo/src/cuda/stereosgm_check_consistency.cu
@@ -24,55 +24,58 @@ namespace cv { namespace cuda { namespace device
     {
         namespace {
             template<typename SRC_T, typename DST_T>
-            __global__ void check_consistency_kernel(DST_T* d_leftDisp, const DST_T* d_rightDisp, const SRC_T* d_left, int width, int height, int src_pitch, int dst_pitch, bool subpixel)  {
+            __global__ void check_consistency_kernel(PtrStep<DST_T> d_leftDisp, const PtrStep<DST_T> d_rightDisp, const PtrStep<SRC_T> d_left, int width, int height, bool subpixel)  {
 
                 const int j = blockIdx.x * blockDim.x + threadIdx.x;
                 const int i = blockIdx.y * blockDim.y + threadIdx.y;
 
                 // left-right consistency check, only on leftDisp, but could be done for rightDisp too
 
-                SRC_T mask = d_left[i * src_pitch + j];
-                int d = d_leftDisp[i * dst_pitch + j];
+                SRC_T mask = d_left(i, j);
+                int d = d_leftDisp(i, j);
                 if (subpixel) {
                     d >>= StereoMatcher::DISP_SHIFT;
                 }
                 int k = j - d;
-                if (mask == 0 || d <= 0 || (k >= 0 && k < width && abs(d_rightDisp[i * dst_pitch + k] - d) > 1)) {
+                if (mask == 0 || d <= 0 || (k >= 0 && k < width && abs(d_rightDisp(i, k) - d) > 1)) {
                     // masked or left-right inconsistent pixel -> invalid
-                    d_leftDisp[i * dst_pitch + j] = 0;
+                    d_leftDisp(i, j) = 0;
                 }
             }
+
+            template <typename disp_type, typename image_type>
+            void check_consistency(PtrStep<disp_type> d_left_disp, const PtrStep<disp_type> d_right_disp, const PtrStep<image_type> d_src_left, int width, int height, bool subpixel, Stream& _stream)
+            {
+                const dim3 blocks(width / 16, height / 16);
+                const dim3 threads(16, 16);
+                cudaStream_t stream = cv::cuda::StreamAccessor::getStream(_stream);
+
+                check_consistency_kernel<image_type, disp_type><<<blocks, threads>>>(d_left_disp, d_right_disp, d_src_left, width, height, subpixel);
+
+                cudaSafeCall( cudaGetLastError() );
+            }
         }
 
-        template <typename disp_type, typename image_type>
-        void check_consistency(PtrStep<disp_type> d_left_disp, const PtrStep<disp_type> d_right_disp, const PtrStep<image_type> d_src_left, int width, int height, bool subpixel);
+        void checkConsistency(GpuMat& left_disp, const GpuMat& right_disp, const GpuMat& src_left, bool subpixel, Stream& stream)
+        {
+            Size size = left_disp.size();
+            CV_Assert(size == right_disp.size());
+            CV_Assert(size == src_left.size());
+            CV_Assert(left_disp.type() == CV_16UC1);
+            CV_Assert(left_disp.type() == right_disp.type());
+            CV_Assert(src_left.type() == CV_8UC1 || src_left.type() == CV_16UC1);
 
-        void check_consistency(uint8_t* d_left_disp, const uint8_t* d_right_disp, const void* d_src_left, int width, int height, int depth_bits, int src_pitch, int dst_pitch, bool subpixel) {
-
-            const dim3 blocks(width / 16, height / 16);
-            const dim3 threads(16, 16);
-            if (depth_bits == 16) {
-                check_consistency_kernel<uint16_t> << < blocks, threads >> > (d_left_disp, d_right_disp, (uint16_t*)d_src_left, width, height, src_pitch, dst_pitch, subpixel);
+            switch (src_left.type())
+            {
+            case CV_8UC1:
+                check_consistency<uint16_t, uint8_t>(left_disp, right_disp, src_left, size.width, size.height, subpixel, stream);
+                break;
+            case CV_16UC1:
+                check_consistency<uint16_t, uint16_t>(left_disp, right_disp, src_left, size.width, size.height, subpixel, stream);
+                break;
+            default:
+                CV_Error(cv::Error::BadDepth, "Unsupported depth");
             }
-            else if (depth_bits == 8) {
-                check_consistency_kernel<uint8_t> << < blocks, threads >> > (d_left_disp, d_right_disp, (uint8_t*)d_src_left, width, height, src_pitch, dst_pitch, subpixel);
-            }
-
-            cudaSafeCall( cudaGetLastError() );
-        }
-
-        void check_consistency(uint16_t* d_left_disp, const uint16_t* d_right_disp, const void* d_src_left, int width, int height, int depth_bits, int src_pitch, int dst_pitch, bool subpixel) {
-
-            const dim3 blocks(width / 16, height / 16);
-            const dim3 threads(16, 16);
-            if (depth_bits == 16) {
-                check_consistency_kernel<uint16_t> << < blocks, threads >> > (d_left_disp, d_right_disp, (uint16_t*)d_src_left, width, height, src_pitch, dst_pitch, subpixel);
-            }
-            else if (depth_bits == 8) {
-                check_consistency_kernel<uint8_t> << < blocks, threads >> > (d_left_disp, d_right_disp, (uint8_t*)d_src_left, width, height, src_pitch, dst_pitch, subpixel);
-            }
-
-            cudaSafeCall( cudaGetLastError() );
         }
 
     }
